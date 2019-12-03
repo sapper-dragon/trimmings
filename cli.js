818f9463d17b2c0d46d8c18a8e87f950c42402ec
Robert Hall <thebookofrobert@gmail.com>
2019-12-02 22:45:00 -0700

    Fixed `false` folder; env options

--- cli.js
+++ cli.js
@@ -1,45 +1,51 @@
+import fs from 'fs-extra'
 import sade from 'sade'
-import { green, red, gray } from 'ansi-colors'
+import { green, yellow, red, gray } from 'ansi-colors'
 import { loadConfig } from './config'
-const exec = require('util').promisify(require('child_process').exec)
 import * as pkg from './package.json'
 import { refresh } from './tools'
 import { setWatch } from './watcher'
 
 const prog = sade('trim').version(pkg.version)
-const potentialSettings = ['input', 'outputSvelte', 'outputStatic']
+const potential_settings = ['input', 'outputSvelte', 'outputStatic']
+const white_list = ['postcss', 'svgo', 'remark', 'lilypond', 'json']
 
 prog
 	.command('watch')
 	.describe('Watch various Sapper "trimming" compilers')
+	.option('-e, --env', 'Use .env file')
 	.option('-p, --postcss', 'Use PostCSS for preprocessing', false)
 	.option('-s, --svgo', 'Use SVGO compiler for svgs', false)
 	.option('-r, --remark', 'Use Remark compiler for markdown', false)
 	.option('-l, --lilypond', 'Use LilyPond compiler for sheet music', false)
 	.option('-j, --json', 'Export generated *.json files from *.js files', false)
 	.action(async(opts) => {
-
 		console.log()
 
+		if (opts.env) {
+			require('dotenv').config({ path: opts.env })
+			console.log('~>', yellow('Preloading environment variables from:'), opts.env)
+		}
+
 		const config = loadConfig()
-		// console.log(config)
 
 		try {
 			await refresh()
 			// here we:
-			for (let name in opts) {
-				// 1. check if the named option is set (i.e., postcss, svgo, etc.)
-				if (name.length > 1 && opts[name]) {
-					console.log('~>', green('Watching Sapper trimming:'), name)
-					for (let potentialDir in config[name]) {
-						// 2. check if `mkdir` needs to be employed (for different dirs)
-						if (potentialSettings.indexOf(potentialDir) > -1) {
-							// 3. if so, do it...
-							await exec(`mkdir -p ${config[name][potentialDir]}`)
+			// 1. check if the named option is set and white listed (i.e., postcss, svgo, etc.)
+			for (let name of white_list.filter(item => opts[item])) {
+				console.log('~>', green('Watching Sapper trimming:'), name)
+				for (let potentialDir in config[name]) {
+					// 2. check if `mkdir` needs to be employed (for different dirs)
+					if (potential_settings.indexOf(potentialDir) > -1) {
+						// 3. if so, do it...
+						const dir = config[name][potentialDir]
+						if (dir) {
+							await fs.ensureDir(dir)
 						}
 					}
-					setWatch(name, config)
 				}
+				setWatch(name, config)
 			}
 		} catch (err) {
 			console.log(red(`> ${err.message}`))
@@ -48,7 +54,6 @@ prog
 		}
 
 		console.log()
-
 	})
 
 prog.parse(process.argv)
